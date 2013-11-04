7c2acf838812c06ac295af9531987e72335ad4c1
ealgruppe <dsafkjh21341@mailinator.com>
2013-11-04 12:33:41 +0100

    Guestbook solution

---
+++ GuestbookEntry/GuestbookEntry/Entry.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestbookEntry
+{
+    public class Entry
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            private set { _id = value; }
+        }
+        private string _text;
+
+        public string Text
+        {
+            get { return _text; }
+            private set { _text = value; }
+        }
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            private set { _name = value; }
+        }
+        private int _rating;
+
+        public int Rating
+        {
+            get { return _rating; }
+            private set { _rating = value; }
+        }
+        public Entry(int id, string text, string name, int rating)
+        {
+            Id = id;
+            Name = name;
+            Text = text;
+            Rating = rating;
+
+        }
+
+    }
+}
