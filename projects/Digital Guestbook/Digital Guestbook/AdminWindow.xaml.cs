d29e3dd9431e12373827d2bde98b0cb80a9b0f77
Mikkel Caira <mikkel@caira.dk>
2013-11-13 10:34:53 +0100

    AdminWindow & Most of the UI
    Added the AdminWindows and added most of the UI, however functionality
    still required!

--- projects/Digital Guestbook/Digital Guestbook/AdminWindow.xaml.cs
+++ projects/Digital Guestbook/Digital Guestbook/AdminWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,19 +11,135 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
-namespace Guestbook_Viewer
-{
+namespace Digital_Guestbook {
 	/// <summary>
-	/// Interaction logic for MainWindow.xaml
+	/// Interaction logic for AdminWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
+	public partial class AdminWindow : Window {
+
+		#region Fields
+		private string fileName = AppDomain.CurrentDomain.BaseDirectory + "Guestbook1.xml";
+		private List<Entry> _gbMsgList = new List<Entry>();
+		#endregion
+
+		public AdminWindow() {
 			InitializeComponent();
+
+			// Loading the Entry List
+			LoadGuestbookFile(fileName);
+
+			foreach(Entry entry in _gbMsgList) {
+				//MessageBox.Show(entry.Name.ToString());
+				//admMessageList.Items.Add(postListBuilder(entry));
+				admMessageList.Children.Add(postListBuilder(entry));
+			}
+
 		}
+
+		public void LoadGuestbookFile(string fileName) {
+			
+			if (!File.Exists(fileName)){
+				//createGuestbookFile(fileName);
+			}
+			else {
+
+				XmlDocument	fileDocument = new XmlDocument();
+							fileDocument.Load(fileName);
+
+				XmlNode		parentNode = fileDocument.DocumentElement;
+				XmlNodeList	entryNodeList = parentNode.SelectNodes("Entry");
+
+				foreach (XmlNode node in entryNodeList){
+					
+					int id;
+					
+					if(!int.TryParse(node["ID"].InnerText, out id)){
+						// Error
+					}
+
+					string		name = node["Name"].InnerText;
+					//string		mail = node["Email"].InnerText;
+					string		text = node["Text"].InnerText;
+					DateTime		dateTime = DateTime.Parse(node["DateTime"].InnerText);
+
+					int rating;
+
+					if (!int.TryParse(node["Rating"].InnerText, out rating)){
+						// Error
+					}
+
+					_gbMsgList.Add(new Entry(id, text, name, rating, dateTime));
+					
+				}
+			}
+		}
+
+		public Grid postListBuilder(Entry entry){
+			
+			ColumnDefinition	column1 = new ColumnDefinition();
+							column1.Width = new GridLength(10, GridUnitType.Star);
+
+			ColumnDefinition	column2 = new ColumnDefinition();
+							column2.Width = new GridLength(85, GridUnitType.Star);
+
+			ColumnDefinition	column3 = new ColumnDefinition();
+							column3.Width = new GridLength(120, GridUnitType.Star);
+
+			ColumnDefinition	column4 = new ColumnDefinition();
+							column4.Width = new GridLength(120, GridUnitType.Star);
+
+			Grid				mainGrid = new Grid();
+							mainGrid.ColumnDefinitions.Add(column1);
+							mainGrid.ColumnDefinitions.Add(column2);
+							mainGrid.ColumnDefinitions.Add(column3);
+							mainGrid.ColumnDefinitions.Add(column4);
+							mainGrid.Margin = new Thickness(0,1,0,0);
+
+			Grid				checkBoxGrid = new Grid();
+							checkBoxGrid.Margin = new Thickness(1,0,0,0);
+							checkBoxGrid.Background = new SolidColorBrush(Color.FromArgb(255,187,187,187));
+							checkBoxGrid.SetValue(Grid.ColumnProperty, 0);
+
+			CheckBox			checkBox = new CheckBox();
+							checkBox.VerticalAlignment = VerticalAlignment.Center;
+							checkBox.HorizontalAlignment = HorizontalAlignment.Center;
+							checkBoxGrid.Children.Add(checkBox);
+
+			TextBlock			nameBlock = new TextBlock();
+							nameBlock.Background = new SolidColorBrush(Color.FromArgb(255,187,187,187));
+							nameBlock.Margin = new Thickness(1,0,0,0);
+							nameBlock.Padding = new Thickness(5,2,5,2);
+							nameBlock.SetValue(Grid.ColumnProperty, 1);
+							nameBlock.Text = entry.Name.ToString();
+							nameBlock.Height = mainGrid.Height;
+
+			TextBlock			mailBlock = new TextBlock();
+							mailBlock.Background = new SolidColorBrush(Color.FromArgb(255,187,187,187));
+							mailBlock.Margin = new Thickness(1,0,0,0);
+							mailBlock.Padding = new Thickness(5,2,5,2);
+							mailBlock.SetValue(Grid.ColumnProperty, 2);
+							mailBlock.Text = "test@email.dk";
+							mailBlock.Height = mainGrid.Height;
+
+			TextBlock			timeBlock = new TextBlock();
+							timeBlock.Background = new SolidColorBrush(Color.FromArgb(255,187,187,187));
+							timeBlock.Margin = new Thickness(1,0,0,0);
+							timeBlock.Padding = new Thickness(5,2,5,2);
+							timeBlock.SetValue(Grid.ColumnProperty, 3);
+							timeBlock.Text = entry.DateTime.ToLongDateString();
+							timeBlock.Height = mainGrid.Height;
+							
+							mainGrid.Children.Add(checkBoxGrid);
+							mainGrid.Children.Add(nameBlock);
+							mainGrid.Children.Add(mailBlock);
+							mainGrid.Children.Add(timeBlock);
+			
+			return mainGrid;
+
+		}
+
 	}
 }
--- projects/Digital Guestbook/Digital Guestbook/MainWindow.xaml.cs
+++ projects/Digital Guestbook/Digital Guestbook/MainWindow.xaml.cs
@@ -14,134 +14,141 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Digital_Guestbook
-{
+{	
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
-    {
-        #region Fields
+	public partial class MainWindow : Window {
+	   #region Fields
 
-        private Guestbook _currentGuestbook;
+	   private Guestbook _currentGuestbook;
 
-        private bool _textCleared;
+	   private bool _textCleared;
 
-        #endregion
+	   public AdminWindow adminWindow = new AdminWindow();
 
-        #region Constructors
+	   #endregion
 
-        public MainWindow()
-        {
-            InitializeComponent();
+	   #region Constructors
 
-            _textCleared = false;
+	   public MainWindow()
+	   {
+		  InitializeComponent();
 
-            _currentGuestbook = new Guestbook();
-            _currentGuestbook.LoadGuestbookFile("Guestbook1.xml");
-            updatePageButtons();
-            updateEntriesView();
-        } 
+		  _textCleared = false;
 
-        #endregion
+		  _currentGuestbook = new Guestbook();
+		  _currentGuestbook.LoadGuestbookFile("Guestbook1.xml");
+		  updatePageButtons();
+		  updateEntriesView();
+	   } 
 
-        #region Public Methods
+	   #endregion
 
-        //public static DateTime TimeStampToDateTime(double timestamp)
-        //{
-        //    // Unix timestamp is seconds past epoch
-        //    System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0);
-        //    dtDateTime = dtDateTime.AddSeconds(timestamp).ToLocalTime();
+	   #region Public Methods
 
-        //    return dtDateTime;
-        //}
+	   //public static DateTime TimeStampToDateTime(double timestamp)
+	   //{
+	   //    // Unix timestamp is seconds past epoch
+	   //    System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+	   //    dtDateTime = dtDateTime.AddSeconds(timestamp).ToLocalTime();
 
-        //public static double ConvertToTimestamp(DateTime value)
-        //{
-        //    //create Timespan by subtracting the value provided from
-        //    //the Unix Epoch
-        //    TimeSpan span = (value - new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime());
+	   //    return dtDateTime;
+	   //}
 
-        //    //return the total seconds (which is a UNIX timestamp)
-        //    return (double)span.TotalSeconds;
-        //} 
+	   //public static double ConvertToTimestamp(DateTime value)
+	   //{
+	   //    //create Timespan by subtracting the value provided from
+	   //    //the Unix Epoch
+	   //    TimeSpan span = (value - new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime());
 
-        #endregion
+	   //    //return the total seconds (which is a UNIX timestamp)
+	   //    return (double)span.TotalSeconds;
+	   //} 
 
-        #region Private methods
+	   #endregion
 
-        // Metoden skal returnere en rating alt efter hvor mange stjerner der er valgt
-        private int calculateRating()
-        {
-            // To do..
-            return 0;
-        }
+	   #region Private methods
 
-        // Metoden tager alt det content der er i en richtextbox og returnerer det som en string
-        private string getStringFromRichTextBox(RichTextBox rtb)
-        {
-            var textRange = new TextRange(rtb.Document.ContentStart, rtb.Document.ContentEnd);
-            return textRange.Text;
-        }
+	   // Metoden skal returnere en rating alt efter hvor mange stjerner der er valgt
+	   private int calculateRating()
+	   {
+		  // To do..
+		  return 0;
+	   }
 
-        private void updateEntriesView()
-        {
-            gbContent.Children.Clear();
+	   // Metoden tager alt det content der er i en richtextbox og returnerer det som en string
+	   private string getStringFromRichTextBox(RichTextBox rtb)
+	   {
+		  var textRange = new TextRange(rtb.Document.ContentStart, rtb.Document.ContentEnd);
+		  return textRange.Text;
+	   }
 
-            foreach (UIElement element in _currentGuestbook.GetGuestbookUIElements())
-            {
-                gbContent.Children.Add(element);
-            }
-        }
+	   private void updateEntriesView()
+	   {
+		  gbContent.Children.Clear();
 
-        private void updatePageButtons()
-        {
-            btnPrevPage.IsEnabled = (_currentGuestbook.CurrentPage != 0);
-            btnNextPage.IsEnabled = (_currentGuestbook.CurrentPage != _currentGuestbook.LastPage);
-        }
+		  foreach (UIElement element in _currentGuestbook.GetGuestbookUIElements())
+		  {
+			 gbContent.Children.Add(element);
+		  }
+	   }
 
-        #endregion
+	   private void updatePageButtons()
+	   {
+		  btnPrevPage.IsEnabled = (_currentGuestbook.CurrentPage != 0);
+		  btnNextPage.IsEnabled = (_currentGuestbook.CurrentPage != _currentGuestbook.LastPage);
+	   }
 
-        #region Events
+	   #endregion
 
-        //Click event for 'Send besked'-knappen der tilføjer en ny Entry til listen 'entryList' der er et field
-        // i main-klassen.
-        private void sendMessageButton_Click(object sender, RoutedEventArgs e)
-        {
-            _currentGuestbook.Add(new Entry(-1, getStringFromRichTextBox(writeNewMessageTextBox).Trim(), writeMessageNameTextBox.Text, calculateRating(), DateTime.Now));
-            updatePageButtons();
-            updateEntriesView();
+	   #region Events
 
-            _currentGuestbook.SaveGuestbookFile("Guestbook1.xml");
+	   //Click event for 'Send besked'-knappen der tilføjer en ny Entry til listen 'entryList' der er et field
+	   // i main-klassen.
+	   private void sendMessageButton_Click(object sender, RoutedEventArgs e)
+	   {
+		  _currentGuestbook.Add(new Entry(-1, getStringFromRichTextBox(writeNewMessageTextBox).Trim(), writeMessageNameTextBox.Text, calculateRating(), DateTime.Now));
+		  updatePageButtons();
+		  updateEntriesView();
 
-            MessageBox.Show("Din besked er blevet tilføjet.", "Besked tilføjet", MessageBoxButton.OK, MessageBoxImage.Information);
+		  _currentGuestbook.SaveGuestbookFile("Guestbook1.xml");
 
-            writeMessageNameTextBox.Clear();
-            writeNewMessageTextBox.Document.Blocks.Clear();
-        }
+		  MessageBox.Show("Din besked er blevet tilføjet.", "Besked tilføjet", MessageBoxButton.OK, MessageBoxImage.Information);
 
-        private void writeNewMessageTextBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            if (!_textCleared)
-            {
-                writeNewMessageTextBox.Document.Blocks.Clear();
-                _textCleared = true;
-            }
-        }
+		  writeMessageNameTextBox.Clear();
+		  writeNewMessageTextBox.Document.Blocks.Clear();
+	   }
 
-        private void btnPrevPage_Click(object sender, RoutedEventArgs e)
-        {
-            _currentGuestbook.CurrentPage--;
-            updatePageButtons();
-            updateEntriesView();
-        }
+	   private void writeNewMessageTextBox_GotFocus(object sender, RoutedEventArgs e)
+	   {
+		  if (!_textCleared)
+		  {
+			 writeNewMessageTextBox.Document.Blocks.Clear();
+			 _textCleared = true;
+		  }
+	   }
 
-        private void btnNextPage_Click(object sender, RoutedEventArgs e)
-        {
-            _currentGuestbook.CurrentPage++;
-            updatePageButtons();
-            updateEntriesView();
-        }
+	   private void btnPrevPage_Click(object sender, RoutedEventArgs e)
+	   {
+		  _currentGuestbook.CurrentPage--;
+		  updatePageButtons();
+		  updateEntriesView();
+	   }
 
-        #endregion
-    }
+	   private void btnNextPage_Click(object sender, RoutedEventArgs e)
+	   {
+		  _currentGuestbook.CurrentPage++;
+		  updatePageButtons();
+		  updateEntriesView();
+	   }
+
+	   #endregion
+
+	   private void openAdminWindow(object sender, RoutedEventArgs e) {
+		   // AdminWindow, Showing
+		   adminWindow.Show();
+	   }
+
+	}
 }
